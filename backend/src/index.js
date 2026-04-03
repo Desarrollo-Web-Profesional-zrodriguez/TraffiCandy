f201bfcde728eee61592e8f10c037f6e90af960d
Miguel Angel Alvarez Ibarra <mijelito333@gmail.com>
2026-04-03 16:23:36 -0600

    fix: Cambios Finales Carrito

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import { connectDB } from './db/db.js'
 import authRoutes from './routes/auth.routes.js'
 import productosRoutes from './routes/productos.routes.js'
 import { notFound as notFoundResponse, serverError } from './utils/httpResponse.js'
+import ordenesRoutes from './routes/ordenes.routes.js'
 
 dotenv.config()
 // Reloading dotenv
@@ -21,6 +22,7 @@ app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/productos', productosRoutes)
+app.use('/api/ordenes', ordenesRoutes)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
