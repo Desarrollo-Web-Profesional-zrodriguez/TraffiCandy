dee853b6bf3c3450374d9bc1f6779f1ab32c1432
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-04-06 01:33:18 -0600

    fix: envio de email por puerto ipV4

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,12 +1,17 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER || "trafficandyconsult@gmail.com",
-    pass: process.env.EMAIL_PASS || "lmfz sfcp mbxb ikob"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
-})
+});
 
 // ── 2FA ─────────────────────────────────────────────────────────────
 export const enviarCorreo2FA = async (email, codigo) => {
