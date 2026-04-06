2ec74b4b838181ba9a7ca87f0a169036732a0bd9
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-04-06 01:53:25 -0600

    fix: detalles para subir la v4 a prod

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   secure: false,
   requireTLS: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER || "trafficandyconsult@gmail.com",
+    pass: process.env.EMAIL_PASS || "lmfz sfcp mbxb ikob"
   },
   tls: {
     rejectUnauthorized: false
