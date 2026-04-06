22ce51eb9916618955cda4f0b69715382036901c
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-04-06 02:09:07 -0600

    fix: detalles para subir la v6 a prod

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -4,12 +4,11 @@ dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS 
+    user: process.env.EMAIL_USER || "trafficandyconsult@gmail.com",
+    pass: process.env.EMAIL_PASS || "lmfz sfcp mbxb ikob"
   },
   tls: {
     rejectUnauthorized: false
